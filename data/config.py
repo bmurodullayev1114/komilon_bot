f93fa76c46d300c22ef38aaa07c9965ae6a25b80
bmurodullayev1114 <bmurodullayev1114@gmail.com>
2022-03-12 11:30:39 +0500

    yangi
    yangi

--- data/config.py
+++ data/config.py
@@ -1,15 +1,17 @@
 # from environs import Env
 #
 # # environs kutubxonasidan foydalanish
-# env = Env()
-# env.read_env()
-#
-# # .env fayl ichidan quyidagilarni o'qiymiz
-# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-# IP = env.str("ip")  # Xosting ip manzili
-#
-# CHANNELS = ["@komilon_savdo"]
+from environs import Env
+
+env = Env()
+env.read_env()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+IP = env.str("ip")  # Xosting ip manzili
+
+CHANNELS = ["@komilon_savdo"]
 #
 import os
 
